bbc29c23f734eddd4a8aee69f159475c27b0422a
lsmite27 <lsmite27@rondebosch.com>
2024-08-05 14:10:42 +0200

    Update assets.ts

--- assets.ts
+++ assets.ts
@@ -1,4 +1,21 @@
 namespace projectImages {
     //% fixedInstance
-    export const pixilframe0 = image.ofBuffer(hex`e410100000000000f000000000000000ef0ff0ffff00fff0cffffffcefffccbfcccfccfcbfccccbfccccccfcbfcbc5ccccccffffcfcbc5bcccfc0000cfcbcccccbfc0f00cfcc8cc8ccccffffffcf88cc88cccefc00ffcc8cc8ceecfc00f0cfffccffffff0000ff0fff000f0000000000f000000000000000000000000000000000000000`);
+    export const const MainHero = img`
+    0 0 f f f f f f f f 0 0 0 0 0 0
+    0 0 f e b b c c c f 0 0 0 0 0 0
+    0 0 0 f c b b b c f f 0 0 0 0 0
+    0 0 0 f c c c c c c f f 0 0 0 0
+    0 0 f c c 5 5 c c 8 c f f 0 0 0
+    0 0 f c c c c c 8 8 c c f 0 0 0
+    0 0 0 f f c c c 8 c c f f 0 0 0
+    0 0 f b b c b c c c 8 f 0 0 0 0
+    0 f f c c c c b c 8 8 c f 0 0 0
+    f e c c c c c c c 8 c c f f 0 0
+    0 f f f c c c c c c e f 0 0 0 0
+    0 0 f c c c f f c c c f 0 0 0 0
+    0 0 f c c f 0 f f e c f f 0 0 0
+    0 f f c c f 0 0 f c e f 0 0 0 0
+    0 f c c c f 0 0 f c c f 0 0 0 0
+    0 f f f f f 0 0 f f f f 0 0 0 0
+    `;
 }
